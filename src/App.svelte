35acc554da4c730df7761ac32448df13d4a3a664
Michel Lesoinne <michel.lesoinne@androdevllc.com>
2021-01-11 08:46:36 -0700

    Add outline and input for new text position and colors.

--- src/App.svelte
+++ src/App.svelte
@@ -5,13 +5,19 @@
 	import ObjectSpinZoom from './ThreeD/ObjectSpinZoom.svelte';
 	export let name;
 
+	let outlineColor = '#ff0000';
+	let bodyColor = '#0000ff';
+
+	let position = new Vector3(Math.random()-0.5, Math.random()-0.5, Math.random()-0.5).normalize();
+
+	$: console.log('Outline color:', outlineColor);
 	let objects = [
-		{text: `Hello ${name}`, position: new Vector3(0,0,0) }
+		{text: `Hello ${name}`, position: new Vector3(0,0,0), bodyColor, outlineColor }
 	];
 	function click(e) {
-		const position = new Vector3(Math.random()-0.5, Math.random()-0.5, Math.random()-0.5).normalize();
-		objects.push( {text: name, position});
+		objects.push( {text: name, position, bodyColor, outlineColor});
 		objects = objects;
+		position = new Vector3(Math.random()-0.5, Math.random()-0.5, Math.random()-0.5).normalize();
 	}
 </script>
 
@@ -20,28 +26,67 @@
 		<ThreeDView >
 			<ObjectSpinZoom>
 				{#each objects as object}
-				<Text text={object.text}  position={object.position} fontSize={0.2}/>
+				<Text text={object.text}  
+				color={object.bodyColor}
+				outlineColor={object.outlineColor}
+				fontSize={0.2}
+				outlineWidth={0.01}
+				position={object.position} 
+				/>
 				{/each}
 			</ObjectSpinZoom>
 		</ThreeDView>
 	</div>
-	<button type="button" on:click={click}>Click Me!</button>
+	<label for="text">Text:</label>
+	<input type="text" id="text" bind:value={name}>
+	<label for='x'>x</label>  
+	<input type='number' id='x' bind:value={position.x}>
+	<label for='y'>y</label>  
+	<input type='number' id='x' bind:value={position.y}>
+	<label for='z'>z</label>  
+	<input type='number' id='x' bind:value={position.z}>
+	<p>Choose the text colors:</p>
+	<div class='colorchoices'>
+		<label for="body">Text body</label>
+    	<input type="color" id="body" name="body" bind:value={bodyColor}>
+		<label for="outline">Outline</label>
+   		<input type="color" id="outline" name="outline" bind:value={outlineColor}>
+		<button type="button" on:click={click}>Add Text</button>
+	</div>
 </main>
 
 <style>
 	main {
-		text-align: center;
-		padding: 1em;
-		max-width: 240px;
+		text-align: left;
+		padding: 0.1em;
 		margin: 0 auto;
 	}
 
+	#body {
+		height: 36px;
+		margin: 4px;
+		padding: 0;
+	}
+	#outline {
+		height: 36px;
+		margin: 4px;
+		padding: 0;
+	}
+	button {
+		padding: 8px;
+		margin: 8px
+	}
 	.view3d {
 		width: 80%;
 		height: 600px;
 		margin: 16px;
 		display: flex;
 		background-color: green;
+	}
+	.colorchoices {
+		display: flex;
+		flex-direction: horizontal;
+		align-items: center;
 	}
 
 	@media (min-width: 640px) {
